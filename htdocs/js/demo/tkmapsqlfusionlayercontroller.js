2b019de2da96429358f6d0279b2a63b66fdffeb5
Tom Kompare <tom@kompare.us>
2012-04-26 15:37:39 -0500

    [jslibrary] more work on SQL API layer

--- htdocs/js/demo/tkmapsqlfusionlayercontroller.js
+++ htdocs/js/demo/tkmapsqlfusionlayercontroller.js
@@ -1,12 +1,11 @@
 /**
- * Using 'tkmap.2.1.0.js' and 'tkmpafusionlayer.2.0.0.js' 
+ * Using 'tkmap.2.1.0.js' and 'tkmapsqlfusionlayer.0.1.0.js' 
  */
-google.load('visualization', '1', {});
 $(document).ready(function() {
 	// The Google Maps base map layer
 	var Map = new TkMap(true,'map',42.01048,-87.6652,15,true,'road');
 	// The tree trim request map layer
-	var TreeTrimLayer = new TkMapSqlFusionLayer(true,Map.map,'3676987','Latitude,Longitude','StreetAddress,ServiceRequestNumber,Status,CreationDate,CompletionDate');
+	var TreeTrimLayer = new TkMapSqlFusionLayer(true,Map.map,'3676987','Latitude,Longitude','StreetAddress,ServiceRequestNumber,Status,CreationDate,CompletionDate','/img/o.png','image');
 //	var TreeTrimLayer = new TkMapSqlFusionLayer(true,Map.map,'3028961','Latitude,Longitude','Status,CompletionDate');
 //	var TreeTrimLayer = new TkMapSqlFusionLayer(true,Map.map,'3676987','Latitude,Longitude','Status,CompletionDate');
 
--- htdocs/js/tkmapsqlfusionlayer/tkmapsqlfusionlayer.0.1.0.js
+++ htdocs/js/tkmapsqlfusionlayer/tkmapsqlfusionlayer.0.1.0.js
@@ -1,18 +1,21 @@
 /**
- * @fileoverview Google Fusion Data Map Layer
- * @author tom@komapre.us (Tom Kompare)
+ * @fileoverview Google Fusion SQL API Data Map Layer
+ * @author tom@kompare.us (Tom Kompare)
  * @package tom@komapre.usJsClass
  * @version 1.0.0
  */
 /**
- * Class for putting Google Fusion Tables data on a Google Maps map.
+ * Class for putting Google Fusion Tables SQL API data on a Google Maps map
  * @requires google.maps JS API v3
  * @param {boolean} showNow - Should FT data display upon object instantiation?
  * @param {object} Map - The Google Maps map object
  * @param {string} fusionTableID - the ID of the Fusion Table
- * @param {string} icon - The column name holding the icon URL.
+ * @param {string} latLngCols - the name of the latitude and longitude columns, separated by a comma
+ * @param {string} dataCols - the comma-separated list of metadata columns
+ * @param {string} icon - The column name holding the icon name
+ * @param {string} iconType - the type of icon name: URL or column
  */
-function TkMapSqlFusionLayer (showNow,Map,fusionTableID,latLngCols,popupCols,icon,iconType) {
+function TkMapSqlFusionLayer (showNow,Map,fusionTableID,latLngCols,dataCols,icon,iconType) {
 	/* Set Default parameters if not defined ***********************************/
 	icon = typeof icon !== 'undefined' ? icon : null;
 	iconType = typeof iconType !== 'undefined' ? iconType : null;
@@ -33,7 +36,7 @@ function TkMapSqlFusionLayer (showNow,Map,fusionTableID,latLngCols,popupCols,ico
 	 */
 	this.fusionTableID = fusionTableID;
 	this.latLngCols = latLngCols;
-	this.popupCols = popupCols;
+	this.dataCols = dataCols;
 	/**
 	 * Name of the icon
 	 * @type string
@@ -50,12 +53,9 @@ function TkMapSqlFusionLayer (showNow,Map,fusionTableID,latLngCols,popupCols,ico
 	 */
 	this.layer = null;
 	/**
-	 * Google SQL API data object
-	 * @type object
+	 * Google Maps Marker objects
+	 * @type array of object
 	 */
-	this.FTTable = null;
-	this.numCols = null;
-	this.numRows = null;
 	this.markers = [];
 	/* METHODS *****************************************************************/
 	/**
@@ -72,6 +72,19 @@ function TkMapSqlFusionLayer (showNow,Map,fusionTableID,latLngCols,popupCols,ico
 		string = typeof string !== 'undefined' ? string : null;
 		keyColumn = typeof keyColumn !== 'undefined' ? keyColumn : null;
 		iconURL = typeof iconURL !== 'undefined' ? icon : this.iconColumn;
+		// Build the query string based on wanted columns
+		var queryString = 'SELECT ' + this.latLngCols;
+		if(this.dataCols.length > 0)
+		{
+			queryString += ',' + this.dataCols;
+		}
+		if(this.iconType === 'column')
+		{
+			queryString += ',' + this.icon;
+		}
+		queryString += ' FROM ' + this.fusionTableID;
+		// I am who I am and that's all that I am.
+		var self = this;
 		//this.hideLayer();
 		// Am I displaying a search result?
 		if (type !== null && type === 'search' && keyColumn.length > 0 && string.length > 0)
@@ -99,13 +112,8 @@ function TkMapSqlFusionLayer (showNow,Map,fusionTableID,latLngCols,popupCols,ico
 		}
 		else
 		{
-			var queryString = 'SELECT ' + this.latLngCols + ',' + this.popupCols + ' FROM ' + this.fusionTableID;
-			alert(queryString);
-			var self = this;
 			var url = 'https://www.google.com/fusiontables/api/query?sql=' + encodeURIComponent(queryString) + '&jsonCallback=?';
-			alert(url);
 			$.getJSON(url,function(response) {
-				alert('here');
 				self.handleResponse(response);
 			});
 		}
@@ -113,13 +121,12 @@ function TkMapSqlFusionLayer (showNow,Map,fusionTableID,latLngCols,popupCols,ico
 	this.handleResponse = function(SqlRows)
 	{
 		var markerLatLng = null;
-		alert(SqlRows.table.rows[0][0]);
 		for (var i = 0, row; row = SqlRows.table.rows[i]; i++) {
 			markerLatLng = new google.maps.LatLng(row[0],row[1]);
 			this.markers[i] = new google.maps.Marker({
 				position: markerLatLng,
 				map: this.Map,
-				icon: 'http://localhost:8888/img/o.png'
+				icon: '/img/o.png'
 			});
 		} 
 	};
