7798b56474ccd3736b16cd7104bc1c5dcc74b6f9
tkompare <tom@kompare.us>
2012-05-03 15:18:47 -0500

    [jslibrary] tkmapfusionlayer d&d not use tkmaps.3.0.0.js

--- htdocs/js/demo/tkmapfusionlayercontroller.js
+++ htdocs/js/demo/tkmapfusionlayercontroller.js
@@ -1,12 +1,14 @@
 /**
- * Using 'tkmap.2.1.0.js' and 'tkmpafusionlayer.2.0.0.js' 
+ * Using 'tkmap.3.0.0.js' and 'tkmpafusionlayer.2.0.0.js' 
  */
 $(document).ready(function() {
 	// The Google Maps base map layer
-	var Map = new TkMap(true,'map',42.01048,-87.6652,15,true,'road minlabels grey');
+	var themap = new TkMap({lat:42.01048,lng:-87.6652,domid:'map'});
+	themap.setCustomStyles({styles:'grey minlabels'});
+	themap.initMap();
 	// The tree trim request map layer
-	var TreeTrimLayer = new TkMapFusionLayer(true,Map.map,'3028961','Location');
-	// Open checkbos listener
+	var TreeTrimLayer = new TkMapFusionLayer(true,themap.Map,'3028961','Location');
+	// Open checkbox listener
 	$("#open").click(function() {
 		if ($("#open").is(':checked')) {
 			TreeTrimLayer.showLayer('search','Open','Status');
