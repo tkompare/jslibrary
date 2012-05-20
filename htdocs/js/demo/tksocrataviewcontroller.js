62fc97f3d84f0d25a4b6299a0314d1d698135eb7
tkompare <tom@kompare.us>
2012-05-20 16:09:49 -0500

    [jslibrary] tksocrataview

---
+++ htdocs/js/demo/tksocrataviewcontroller.js
@@ -0,0 +1,28 @@
+var Data = null;
+var BusinessLicenses = new TkSocrataView
+({
+	tableid : 'b7mg-yyns',
+	domain : 'data.cityofchicago.org'
+});
+$("#search-button").click(function(){
+	if(Data === null)
+	{
+		Data = BusinessLicenses.getData();
+	}
+	$('#license-body').empty();
+	$('#alertbox').empty();
+	var pattern = new RegExp($("#number").val()+'[0-9]{2}.*'+$("#name").val(),'i');
+	var matched = 0;
+	for (var i=0; i<Data.length; i++)
+	{
+		if(Data[i].address.match(pattern) !== null)
+		{
+			$('#license-body').append('<tr><td>'+$.trim(Data[i].address)+'</td><td>'+$.trim(Data[i].doing_business_as_name)+'</td><td>'+Data[i].license_description+'</td></tr>');
+			matched++;
+		}
+	}
+	if (matched === 0)
+	{
+		$('#alertbox').append('<div class="alert alert-error">Your search did not find any businesses within Chicago\'s 49th Ward');
+	}
+});
